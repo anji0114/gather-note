7e58e2b6e29e9344fa7e2fe45358bd3060a197fe
anji0114 <anji.tnk.140114@gmail.com>
2023-05-12 10:00:07 +0900

    グループ不参加時の見え方の実装

--- src/pages/group/[id]/board.tsx
+++ src/pages/group/[id]/board.tsx
@@ -6,38 +6,47 @@ import { DateFns } from "@/components/Common/DateFns";
 import { GroupLayout } from "@/components/Group/GroupLayout";
 import { DashboardHeading } from "@/components/Common/Heading";
 import { BoardCreate } from "@/components/Board/BoardCreate";
+import { useGroupMembership } from "@/hooks/useGroupMembership";
+import { GroupRegister } from "@/components/Group/GroupRegister";
 
 const GroupBoardPage = () => {
   const group = useStore((state) => state.group);
 
   const { data, error, isLoading } = useSWR(group.id ? `/api/groups/${group.id}/boards` : null);
+  const { isMember, isLoading: isMemberLoading } = useGroupMembership(group.id);
 
   return (
     <GroupLayout>
       <DashboardHeading text="ボード一覧" icon={<ClipboardDocumentListIcon />}>
-        <BoardCreate />
+        {isMember && <BoardCreate />}
       </DashboardHeading>
 
-      <ul className="mt-8 space-y-[1px]">
-        {data?.map((board: any) => (
-          <li
-            className="dashboard-item01 py-5 px-7 bg-white border border-[#d0d7de]"
-            key={board.id}
-          >
-            <p className="pl-[2px] text-[#555] text-[12px]">
-              <DateFns time={board.created_at} />
-            </p>
-            <p className="mt-2.5">
-              <Link
-                href={`/board/${board.id}`}
-                className="text-[#4e6bb4] font-medium underline-offset-2  hover:underline"
-              >
-                {board.name}
-              </Link>
-            </p>
-          </li>
-        ))}
-      </ul>
+      {isMemberLoading ? (
+        <></>
+      ) : isMember ? (
+        <ul className="mt-8 space-y-[1px]">
+          {data?.map((board: any) => (
+            <li
+              className="dashboard-item01 py-5 px-7 bg-white border border-[#d0d7de]"
+              key={board.id}
+            >
+              <p className="pl-[2px] text-[#555] text-[12px]">
+                <DateFns time={board.created_at} />
+              </p>
+              <p className="mt-2.5">
+                <Link
+                  href={`/board/${board.id}`}
+                  className="text-[#4e6bb4] font-medium underline-offset-2  hover:underline"
+                >
+                  {board.name}
+                </Link>
+              </p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <GroupRegister groupId={group.id} />
+      )}
     </GroupLayout>
   );
 };
