b4fa6413115692348ce5b682f282cc6cd663a0c7
anji0114 <anji.tnk.140114@gmail.com>
2023-05-20 10:33:14 +0900

    スタイルの調整

--- src/components/Board/BoardNotes.tsx
+++ src/components/Board/BoardNotes.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import useSWR from "swr";
-import { DocumentTextIcon, TrashIcon } from "@heroicons/react/24/outline";
+import { DocumentTextIcon, EllipsisVerticalIcon } from "@heroicons/react/24/outline";
 import { Note } from "@/types";
 import { DateFns } from "@/components/Common/DateFns";
 import { useStore } from "@/store";
@@ -32,25 +32,25 @@ export const BoardNotes = () => {
   return (
     <ul className="mt-8 space-y-4">
       {data?.map((note: Note & { user_id: string }) => (
-        <li key={note.id} className="flex justify-between w-full pb-4 border-b border-[#d0d7de]">
+        <li
+          key={note.id}
+          className=" w-full pb-4 border-b border-[#d0d7de] md:flex md:justify-between md:flex-row-reverse"
+        >
+          <p className="flex items-center justify-between gap-2 px-1 text-[12px] md:px-0">
+            <DateFns time={note.updated_at!} />
+            {user?.id === note.user_id && (
+              <button className="w-5 cursor-pointer" onClick={() => handleDeleteNote(note.id)}>
+                <EllipsisVerticalIcon />
+              </button>
+            )}
+          </p>
           <Link
             href={`/note/${note.id}`}
-            className="relative inline-block pl-7 text-[#4e6bb4] text-sm font-medium underline-offset-3 hover:underline"
+            className="relative inline-block pl-7 mt-2 text-[#4e6bb4] text-sm font-medium underline-offset-3 hover:underline md:mt-0"
           >
             <DocumentTextIcon className="absolute left-0 top-1/2 translate-y-[-50%] w-6" />
             <span>{note.name}</span>
           </Link>
-          <p className="text-[12px] flex items-center gap-3">
-            <DateFns time={note.updated_at!} />
-            {user?.id === note.user_id && (
-              <button
-                className="w-4 text-[#DE6868] cursor-pointer"
-                onClick={() => handleDeleteNote(note.id)}
-              >
-                <TrashIcon />
-              </button>
-            )}
-          </p>
         </li>
       ))}
     </ul>
