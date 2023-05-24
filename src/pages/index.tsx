b506879302afb7d9bbb1bffc725676c1ee73e3c1
anji0114 <anji.tnk.140114@gmail.com>
2023-05-24 12:04:27 +0900

    レイアウトの調整・画像変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,57 @@
-import Head from "next/head";
-import { Layout } from "@/components/Layout";
-import { HomeMv } from "@/components/Home/HomeMv";
+import Image from "next/image";
 import { NextPage } from "next";
+import Link from "next/link";
+import { useUser } from "@supabase/auth-helpers-react";
+import { Layout } from "@/components/Layout";
 import { Meta } from "@/components/Common/Meta";
 
 const Home: NextPage = () => {
+  const user = useUser();
+
   return (
     <>
       <Meta />
       <Layout>
-        <HomeMv />
+        <div className="min-h-[calc(100vh_-_150px)] py-10 flex items-center justify-center">
+          <div>
+            <div className="text-center">
+              <Image
+                src="/logo.png"
+                alt="Prompt Note"
+                width={300}
+                height={70}
+                className="inline-block"
+              />
+            </div>
+            <p className="mt-6 text-center font-medium text-lg leading-6 ">
+              1ページずつ、ノートを一緒に集める
+            </p>
+            <div className="flex mt-12 justify-center gap-5">
+              {user ? (
+                <Link
+                  className="w-40 inline-block text-center p-3 text-sm bg-[#222] text-white rounded hover:bg-[#555]"
+                  href="/dashboard"
+                >
+                  ダッシュボード
+                </Link>
+              ) : (
+                <Link
+                  className="w-40 inline-block text-center p-3 text-sm bg-[#222] text-white rounded hover:bg-[#555]"
+                  href="/auth/signup"
+                >
+                  ユーザー登録
+                </Link>
+              )}
+              <Link
+                className="w-40 inline-block text-center p-3 text-sm rounded border border-[#222] bg-white hover:bg-[#eee]"
+                href="https://github.com/anji0114/gather-note"
+                target="_blank"
+              >
+                GitHub
+              </Link>
+            </div>
+          </div>
+        </div>
       </Layout>
     </>
   );
