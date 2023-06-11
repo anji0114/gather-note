d1d4acf4793760d3a06af712749f0588435f7130
anji0114 <anji.tnk.140114@gmail.com>
2023-06-11 14:39:19 +0900

    ナブメニューをmemo化

--- src/components/Dashboard/DashboardLayout.tsx
+++ src/components/Dashboard/DashboardLayout.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode, useState } from "react";
+import { FC, ReactNode, useMemo, useState } from "react";
 import { FolderOpenIcon, UserGroupIcon, Cog8ToothIcon } from "@heroicons/react/24/outline";
 import { GridLayout } from "@/components/Common/GridLayout";
 import { Layout } from "@/components/Layout";
@@ -9,26 +9,29 @@ export const DashboardLayout: FC<{ children: ReactNode }> = ({ children }) => {
   const router = useRouter();
   const asPath = router.asPath;
 
-  const navItems = [
-    {
-      title: "フォルダ",
-      href: "/dashboard",
-      icon: <FolderOpenIcon className="w-[22px]" />,
-      isPage: asPath === "/dashboard",
-    },
-    {
-      title: "グループ",
-      href: "/dashboard/group",
-      icon: <UserGroupIcon className="w-[22px]" />,
-      isPage: asPath === "/dashboard/group",
-    },
-    {
-      title: "設定",
-      href: "/dashboard/setting",
-      icon: <Cog8ToothIcon className="w-[22px]" />,
-      isPage: asPath === "/dashboard/setting",
-    },
-  ];
+  const navItems = useMemo(
+    () => [
+      {
+        title: "フォルダ",
+        href: "/dashboard",
+        icon: <FolderOpenIcon className="w-[22px]" />,
+        isPage: asPath === "/dashboard",
+      },
+      {
+        title: "グループ",
+        href: "/dashboard/group",
+        icon: <UserGroupIcon className="w-[22px]" />,
+        isPage: asPath === "/dashboard/group",
+      },
+      {
+        title: "設定",
+        href: "/dashboard/setting",
+        icon: <Cog8ToothIcon className="w-[22px]" />,
+        isPage: asPath === "/dashboard/setting",
+      },
+    ],
+    [asPath]
+  );
 
   return (
     <Layout classes="py-20 md:py-24">
